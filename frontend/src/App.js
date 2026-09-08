fbf2d8fec6f0d79bc11e480cc92c9555a0dbdd26
Rajyalakshmi676 <rajitella7@gmail.com>
2026-09-08 10:23:29 +0530

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -97,6 +97,8 @@ const DashboardLayout = lazy(() => import('./dashboard/Layout'));
 const Inbox = lazy(() => import('./components/Inbox'));
 const Templates = lazy(() => import('./components/Templates'));
 const Campaigns = lazy(() => import('./components/Campaigns'));
+const Analytics = lazy(() => import('./components/Analytics'));
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -317,6 +319,7 @@ function App() {
         <Route path="/whatsapp/inbox" element={privateRoute('WhatsApp Inbox', <Inbox />)} />
         <Route path="/whatsapp/templates" element={privateRoute('WhatsApp Templates', <Templates />)} />
         <Route path="/whatsapp/campaigns" element={privateRoute('WhatsApp Campaigns', <Campaigns />)} />
+        <Route path="/whatsapp/Analytics" element={privateRoute('WhatsApp Analytics', <Analytics />)} />
         <Route path="/admin/whatsapp/dashboard" element={privateRoute('Admin WhatsApp Dashboard', <AdminWhatsAppDashboard />)} />
         
         {/* SMS Routes */}
